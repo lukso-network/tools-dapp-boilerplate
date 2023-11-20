ceac934186c164bd1db3a4fdac704d62190cb9ec
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-11-20 23:11:26 +0100

    update elements page

--- src/pages/elements.tsx
+++ src/pages/elements.tsx
@@ -1,10 +1,12 @@
 import ConnectButton from '@/components/ConnectButton'
+import NetworkSwitcher from '@/components/NetworkSwitcher/NetworkSwitcher'
 
 export default function Home() {
   return (
     <div className="home-background">
       <div className="flex items-center justify-center h-screen">
         <ConnectButton />
+        <NetworkSwitcher />
       </div>
     </div>
   )
