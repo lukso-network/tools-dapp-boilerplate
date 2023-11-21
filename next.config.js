6f6f7e82132f9b05be28590d22d5e399def57949
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-11-21 21:10:28 +0100

    add profile preview component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    /** Whitelisted domains for optimized image loading
+     *  @next/image
+     */
+    domains: ['api.universalprofile.cloud'],
+  },
+}
 
 module.exports = nextConfig
---
+++ src/components/ProfilePreview/ProfilePreview.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import Image from 'next/image'
+import { useProfile } from '../../contexts/ProfileContext'
+import { useEthereum } from '../../contexts/EthereumContext'
+import identicon from 'ethereum-blockies-base64'
+import styles from './ProfilePreview.module.css'
+
+const ProfilePreview: React.FC = () => {
+  const { profile } = useProfile()
+  const { account } = useEthereum()
+  const identiconUrl = account ? identicon(account) : ''
+
+  return (
+    <div
+      className={`${styles.profileContainer} relative bg-white rounded-lg shadow-lg p-4 mx-auto`}
+    >
+      {profile?.backgroundImage && profile.backgroundImage.length > 0 && (
+        <Image
+          src={profile.backgroundImage[0].url.replace(
+            'ipfs://',
+            'https://api.universalprofile.cloud/ipfs/'
+          )}
+          alt="Background"
+          layout="fill"
+          objectFit="cover"
+          className={`${styles.profileBackground} rounded-lg absolute inset-0 z-0`}
+        />
+      )}
+
+      <div className="flex justify-center relative z-10">
+        <div
+          className={`${styles.profileImage} w-24 h-24 bg-gray-200 rounded-full overflow-hidden relative border-4 border-white`}
+        >
+          {!profile?.profileImage || profile.profileImage.length === 0 ? (
+            <div className="w-full h-full bg-gray-300 rounded-full"></div> // Grey background if no image
+          ) : (
+            <Image
+              src={profile.profileImage[0].url.replace(
+                'ipfs://',
+                'https://api.universalprofile.cloud/ipfs/'
+              )}
+              alt="Profile"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-full"
+            />
+          )}
+        </div>
+        <div
+          className={`${styles.blockie} absolute mb-6 mr-6 bg-gray-200 rounded-full overflow-hidden border-4 border-white`}
+        >
+          {!profile?.profileImage || profile.profileImage.length === 0 ? (
+            <div className="w-full h-full bg-gray-300 rounded-full"></div> // Grey background if no image
+          ) : (
+            <Image
+              src={identiconUrl}
+              alt="Blockie"
+              layout="fill"
+              objectFit="cover"
+              className="rounded-full"
+            />
+          )}
+        </div>
+      </div>
+
+      <div
+        className={`${styles.profileData} text-center mt-4 bg-white p-2 rounded-lg relative z-10`}
+      >
+        <p className="text-lg font-semibold">{profile?.name || 'Anonymous'}</p>
+        <p className="text-sm text-gray-600">{account || '0x'}</p>
+        {profile?.description ? (
+          <p className="text-sm text-gray-500 mt-2">{profile?.description}</p>
+        ) : (
+          <div
+            className={`${styles.emptyDescription} w-full bg-gray-300 mt-2 rounded-lg`}
+          ></div>
+        )}
+
+        {profile?.tags && (
+          <div className="flex justify-center mt-2">
+            {profile.tags.map((tag, index) => (
+              <a
+                key={index}
+                className=" my-1 bg-pink-200 text-pink-800 rounded-full px-2 py-1 mx-1 text-xs"
+              >
+                {tag}
+              </a>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default ProfilePreview
