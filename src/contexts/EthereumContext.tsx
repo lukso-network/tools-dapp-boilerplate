a59d7fcd7cdabb4f365463c7b04d1bf144c141bc
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-11-28 11:39:49 +0100

    Add code documentation to contexts

--- src/contexts/EthereumContext.tsx
+++ src/contexts/EthereumContext.tsx
@@ -15,16 +15,33 @@ const defaultValue: EthereumContextType = {
   disconnect: async () => {},
 }
 
+// Set up the empty React context
 const EthereumContext = createContext<EthereumContextType>(defaultValue)
 
+/**
+ * Custom hook to use the Ethereum context across the application.
+ *
+ * @returns {EthereumContextType} - The provider, account, and connect/disconnect functions.
+ */
 export function useEthereum() {
   return useContext(EthereumContext)
 }
 
+/**
+ * Provider component for the Ethereum context, handling account connectivity and
+ * maintaining its state during account and chain changes.
+ *
+ * @param {React.ReactNode} { children } - Child components using the Ethereum context.
+ * @returns {JSX.Element} - The JSX structure that wraps the child components to provide
+ *                          access to it's state and functionalities.
+ */
+
 export function EthereumProvider({ children }: { children: React.ReactNode }) {
+  // State for the Ethereum provider and the connected account
   const [provider, setProvider] = useState<ethers.BrowserProvider | null>(null)
   const [account, setAccount] = useState<string | null>(null)
 
+  // Initialize the provider and listen for account/chain changes
   useEffect(() => {
     if (window.ethereum) {
       const provider = new ethers.BrowserProvider(window.ethereum)
@@ -34,12 +51,14 @@ export function EthereumProvider({ children }: { children: React.ReactNode }) {
         setAccount(accounts[0] || null)
       })
 
+      // Reload the page when the chain changes
       window.ethereum.on('chainChanged', () => {
         window.location.reload()
       })
     }
   }, [])
 
+  // Connect to the Ethereum network using the user's extension
   const connect = async () => {
     if (!provider) {
       console.log('Provider is not set')
@@ -49,10 +68,11 @@ export function EthereumProvider({ children }: { children: React.ReactNode }) {
       const accounts = await provider.send('eth_requestAccounts', [])
       setAccount(accounts[0])
     } catch (error) {
-      console.log('o accounts given')
+      console.log('User denied connection request')
     }
   }
 
+  // Disconnect the provider by resetting the app's account
   const disconnect = () => {
     setAccount(null)
   }
