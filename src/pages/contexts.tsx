879a48269fc98bfd9c493595d9e36d7b7f447b5f
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2024-04-23 08:38:27 +0200

    remove redundant component tag

--- src/pages/contexts.tsx
+++ src/pages/contexts.tsx
@@ -3,86 +3,79 @@
  */
 export default function Contexts() {
   return (
-    <>
-      <div className="home-background">
-        <div className="flex flex-col items-center justify-center main-content">
-          <h1 className="text-2xl">Built-in Context States:</h1>
-          <div className="rounded-lg border border-red-100 p-5 bg-pink-50 mt-4">
-            <h4 className="text-xl mb-2 font-bold">🔑 Ethereum Provider</h4>
-            <div className="mb-32 lg:mb-0 lg:max-w-3xl opacity-70 text-sm">
-              You can import the <code className="font-bold">useEthereum</code>{' '}
-              context within any page or component in order to globally manage
-              the connected Universal Profile Browser Extension. The{' '}
-              <code className="font-bold">
-                src/contexts/EthereumContext.tsx
-              </code>{' '}
-              can:
-              <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left group mt-4">
-                <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
-                  Get the current browser provider
-                </p>
-                <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
-                  Connect and remove the Universal Profile
-                </p>
-                <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
-                  Act on provider or address changes
-                </p>
-                <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white text-center">
-                  Toggle Web3 Onboard functionality
-                </p>
-              </div>
+    <div className="home-background">
+      <div className="flex flex-col items-center justify-center main-content">
+        <h1 className="text-2xl">Built-in Context States:</h1>
+        <div className="rounded-lg border border-red-100 p-5 bg-pink-50 mt-4">
+          <h4 className="text-xl mb-2 font-bold">🔑 Ethereum Provider</h4>
+          <div className="mb-32 lg:mb-0 lg:max-w-3xl opacity-70 text-sm">
+            You can import the <code className="font-bold">useEthereum</code>{' '}
+            context within any page or component in order to globally manage the
+            connected Universal Profile Browser Extension. The{' '}
+            <code className="font-bold">src/contexts/EthereumContext.tsx</code>{' '}
+            can:
+            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left group mt-4">
+              <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
+                Get the current browser provider
+              </p>
+              <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
+                Connect and remove the Universal Profile
+              </p>
+              <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
+                Act on provider or address changes
+              </p>
+              <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white text-center">
+                Toggle Web3 Onboard functionality
+              </p>
             </div>
           </div>
-          <div className="rounded-lg border border-red-100 p-5 bg-pink-50 mt-4">
-            <h4 className="text-xl mb-2 font-bold">👩‍🎤 Profile Provider</h4>
-            <div className="mb-32 lg:mb-0 lg:max-w-3xl opacity-70 text-sm">
-              You can import the <code className="font-bold">useProfile</code>{' '}
-              context within any page or component in order to globally manage
-              profile data from the Universal Profile Browser Extension. The{' '}
-              <code className="font-bold">
-                src/contexts/EthereumContext.tsx
-              </code>{' '}
-              can:
-              <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left group mt-4">
-                <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
-                  Retrieve and decode metadata of the Universal Profile
-                </p>
-                <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
-                  Store and update information based on provider changes
-                </p>
-                <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white text-center">
-                  Optimize image selection and frontend rendering
-                </p>
-              </div>
+        </div>
+        <div className="rounded-lg border border-red-100 p-5 bg-pink-50 mt-4">
+          <h4 className="text-xl mb-2 font-bold">👩‍🎤 Profile Provider</h4>
+          <div className="mb-32 lg:mb-0 lg:max-w-3xl opacity-70 text-sm">
+            You can import the <code className="font-bold">useProfile</code>{' '}
+            context within any page or component in order to globally manage
+            profile data from the Universal Profile Browser Extension. The{' '}
+            <code className="font-bold">src/contexts/EthereumContext.tsx</code>{' '}
+            can:
+            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left group mt-4">
+              <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
+                Retrieve and decode metadata of the Universal Profile
+              </p>
+              <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
+                Store and update information based on provider changes
+              </p>
+              <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white text-center">
+                Optimize image selection and frontend rendering
+              </p>
             </div>
           </div>
-          <div className="rounded-lg border border-red-100 p-5 bg-pink-50 mt-4">
-            <h4 className="text-xl mb-2 font-bold">🕸️ Network Provider</h4>
-            <div className="mb-32 lg:mb-0 lg:max-w-3xl opacity-70 text-sm">
-              You can import the <code className="font-bold">useNetwork</code>{' '}
-              context within any page or component in order to globally manage
-              the network connection of the Universal Profile Browser Extension.
-              The{' '}
-              <code className="font-bold">src/contexts/NetworkContext.tsx</code>{' '}
-              can:
-              <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left group mt-4">
-                <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
-                  Get the current selected network
-                </p>
-                <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
-                  Check if the network is supported
-                </p>
-                <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
-                  Switch networks or act on changes
-                </p>
-                <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white text-center">
-                  Control related RPCs and IPFS Gateways
-                </p>
-              </div>
+        </div>
+        <div className="rounded-lg border border-red-100 p-5 bg-pink-50 mt-4">
+          <h4 className="text-xl mb-2 font-bold">🕸️ Network Provider</h4>
+          <div className="mb-32 lg:mb-0 lg:max-w-3xl opacity-70 text-sm">
+            You can import the <code className="font-bold">useNetwork</code>{' '}
+            context within any page or component in order to globally manage the
+            network connection of the Universal Profile Browser Extension. The{' '}
+            <code className="font-bold">src/contexts/NetworkContext.tsx</code>{' '}
+            can:
+            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left group mt-4">
+              <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
+                Get the current selected network
+              </p>
+              <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
+                Check if the network is supported
+              </p>
+              <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white mr-2 text-center">
+                Switch networks or act on changes
+              </p>
+              <p className="text-sm rounded-lg border px-5 py-4 border-slate-200 bg-white text-center">
+                Control related RPCs and IPFS Gateways
+              </p>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/pages/utils.tsx
+++ src/pages/utils.tsx
@@ -3,45 +3,43 @@
  */
 export default function Utility() {
   return (
-    <>
-      <div className="home-background">
-        <div className="flex flex-col items-center justify-center main-content">
-          <h1 className="text-2xl">Built-in Utility Tools:</h1>
-          <div className="rounded-lg border border-red-100 p-5 bg-pink-50 mt-4">
-            <h4 className="text-xl mb-2 font-bold">🔎 Interface Detection</h4>
-            <div className="mb-32 lg:mb-0 lg:max-w-2xl opacity-70 text-sm">
-              You can use the{' '}
-              <code className="font-bold">supportsInterface()</code> function
-              within{' '}
-              <code className="font-bold">src/utils/interfaceDetection.ts</code>{' '}
-              to quickly check ERC615 interface IDs of LSP smart contracts you
-              are interacting with.
-            </div>
+    <div className="home-background">
+      <div className="flex flex-col items-center justify-center main-content">
+        <h1 className="text-2xl">Built-in Utility Tools:</h1>
+        <div className="rounded-lg border border-red-100 p-5 bg-pink-50 mt-4">
+          <h4 className="text-xl mb-2 font-bold">🔎 Interface Detection</h4>
+          <div className="mb-32 lg:mb-0 lg:max-w-2xl opacity-70 text-sm">
+            You can use the{' '}
+            <code className="font-bold">supportsInterface()</code> function
+            within{' '}
+            <code className="font-bold">src/utils/interfaceDetection.ts</code>{' '}
+            to quickly check ERC615 interface IDs of LSP smart contracts you are
+            interacting with.
           </div>
-          <div className="rounded-lg border border-red-100 p-5 bg-pink-50 mt-4">
-            <h4 className="text-xl mb-2 font-bold">🗂️ Metadata Detection</h4>
-            <div className="mb-32 lg:mb-0 lg:max-w-2xl opacity-70 text-sm">
-              You can use the{' '}
-              <code className="font-bold">supportsMetadata()</code> function
-              within{' '}
-              <code className="font-bold">src/utils/metadataDetection.ts</code>{' '}
-              to quickly check ERC725Y data keys of LSP smart contracts you are
-              interacting with.
-            </div>
+        </div>
+        <div className="rounded-lg border border-red-100 p-5 bg-pink-50 mt-4">
+          <h4 className="text-xl mb-2 font-bold">🗂️ Metadata Detection</h4>
+          <div className="mb-32 lg:mb-0 lg:max-w-2xl opacity-70 text-sm">
+            You can use the{' '}
+            <code className="font-bold">supportsMetadata()</code> function
+            within{' '}
+            <code className="font-bold">src/utils/metadataDetection.ts</code> to
+            quickly check ERC725Y data keys of LSP smart contracts you are
+            interacting with.
           </div>
-          <div className="rounded-lg border border-red-100 p-5 bg-pink-50 mt-4">
-            <h4 className="text-xl mb-2 font-bold">🔑 Sign In With Ethereum</h4>
-            <div className="mb-32 lg:mb-0 lg:max-w-2xl opacity-70 text-sm">
-              You can use the{' '}
-              <code className="font-bold">signInWithEthereum()</code> function
-              within{' '}
-              <code className="font-bold">src/utils/signInWithEthereum.ts</code>{' '}
-              to quickly prove ownership over the account that you are connected
-              with.
-            </div>
+        </div>
+        <div className="rounded-lg border border-red-100 p-5 bg-pink-50 mt-4">
+          <h4 className="text-xl mb-2 font-bold">🔑 Sign In With Ethereum</h4>
+          <div className="mb-32 lg:mb-0 lg:max-w-2xl opacity-70 text-sm">
+            You can use the{' '}
+            <code className="font-bold">signInWithEthereum()</code> function
+            within{' '}
+            <code className="font-bold">src/utils/signInWithEthereum.ts</code>{' '}
+            to quickly prove ownership over the account that you are connected
+            with.
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
